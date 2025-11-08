1eed17d88a352e8fcdb5e1f07edb72297b726efd
SANKET S HOSALLI <shosalli220599@gmail.com>
2025-11-08 16:34:35 +0530

    chore(frontend): update frontend code

---
+++ next/static/chunks/app/articles/page-b4feb5d14a6717da.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[220,292,379,904,968,977],{2192:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,3549,23))}},e=>{e.O(0,[549,441,826,358],()=>e(e.s=2192)),_N_E=e.O()}]);
---
+++ next/static/chunks/next/dist/client/components/builtin/global-error-20776cb0ef995fd3.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[337],{7691:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,5704,23))}},e=>{e.O(0,[441,826,358],()=>e(e.s=7691)),_N_E=e.O()}]);
